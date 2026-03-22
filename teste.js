6263e39ff70bc5114d4c92194296419f26745a29
Renan Marquetti <renanmarquetti@MacBook-Pro-de-Renan.local>
2026-03-22 14:43:56 -0300

    add test byGame

--- teste.js
+++ teste.js
@@ -4,33 +4,39 @@ const testPosicoes = (posicoes, results, grafoGame) => {
 		for (let y = 0; y < posicoes.length; y++) {
 			const posicaoInicial = posicoes[x];
 			const posicaoFinal = posicoes[y];
-			const game = new Game(posicoes[x], posicoes[y]);
-			const res = game.getResult().trim();
-			const expect = results.get(posicoes[x] + posicoes[y]);
-			const notacao = posicaoInicial + " -> " + posicaoFinal;
-			if (res != expect) {
 
-				const resLength = res.split(" ").length
-				const expectLength = expect.split(" ").length
-				const isCorrectLength = resLength == expectLength;
-
-				//console.log({ resLength, expectLength });
-
-				const posicaoFinalByPath = grafoGame.getPosicaoByPath(posicaoInicial, res).notacao;
-				const isCorrectPosicaoFinal = posicaoFinalByPath === posicaoFinal;
-
-				// console.log({ posicaoFinalByPath, isCorrectPosicaoFinal })
-
-				if (isCorrectLength && isCorrectPosicaoFinal) {
-					continue;
-				}
-				errors.push({ game: notacao, isCorrectLength, expect, res, isCorrectPosicaoFinal, posicaoFinal, posicaoFinalByPath });
-
-			};
+			const result = testPosicao(posicaoInicial, posicaoFinal, results, grafoGame);
+			if (result) errors.push(result);
 		}
 	}
 
 	return errors;
+}
+
+const testPosicao = (posicaoInicial, posicaoFinal, results, grafoGame) => {
+	const game = new Game(posicaoInicial, posicaoFinal);
+	const res = game.getResult().trim();
+	const expect = results.get(posicaoInicial + posicaoFinal);
+	const notacao = posicaoInicial + " -> " + posicaoFinal;
+	if (res != expect) {
+
+		const resLength = res.split(" ").length
+		const expectLength = expect.split(" ").length
+		const isCorrectLength = resLength == expectLength;
+
+		//console.log({ resLength, expectLength });
+
+		const posicaoFinalByPath = grafoGame.getPosicaoByPath(posicaoInicial, res).notacao;
+		const isCorrectPosicaoFinal = posicaoFinalByPath === posicaoFinal;
+
+		// console.log({ posicaoFinalByPath, isCorrectPosicaoFinal })
+
+		if (isCorrectLength && isCorrectPosicaoFinal) {
+			return null;
+		}
+
+		return { game: notacao, isCorrectLength, expect, res, isCorrectPosicaoFinal, posicaoFinal, posicaoFinalByPath };
+	}
 }
 
 const testGrafo = (qtdDisco = 1, lambdaFilter = (e) => true) => {
@@ -50,5 +56,25 @@ const testGrafo = (qtdDisco = 1, lambdaFilter = (e) => true) => {
 	const errors = testPosicoes(posicoes, results, grafoGame).filter(lambdaFilter);
 	console.log(errors);
 
+}
+
+const testGrafoByGame = (qtdDisco = 1, notacaoGame) => {
+	const game = notacaoGame.split(" ");
+	const init = game[0]
+	const fim = game[2]
+
+	console.log({ game, init, fim });
+
+	const grafoGame = new Grafo(qtdDisco);
+
+	let results = new Map();
+
+	grafoGame.resetBalanceGrafo();
+	grafoGame.balanceGrafoByNotacao(init)
+	results.set(init + fim, grafoGame.getPatchByNotacao(fim).trim());
+	console.log({ results });
+
+	const errors = testPosicao(init, fim, results, grafoGame);
+	console.log(errors);
 }
 
